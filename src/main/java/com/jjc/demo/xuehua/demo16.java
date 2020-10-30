0d7a280ff1dceeff4538c420c93107e49255f613
jjc <457375302@qq.com>
2020-10-30 14:38:11 +0800

    测试

--- src/main/java/com/jjc/demo/xuehua/demo16.java
+++ src/main/java/com/jjc/demo/xuehua/demo16.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class demo16 {
     public static void main(String[] args) {
+        System.out.println("ces");
         //String stringStream = Arrays.stream(list.stream()).map(i -> i.toString()).collect(Collectors.joining(","));
     }
 }
